6325680030afecbf0915d222893d9330ae2858e0
roa-develop <57290818+roa-develop@users.noreply.github.com>
2019-11-02 17:54:51 +0300

    primary commit
    primary commit

---
+++ CompRepairShop/CompRepairShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompRepairShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
